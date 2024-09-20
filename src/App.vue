4b13646c9186f637e53902c7bfb7a476767443e8
Giovanni Zumbo <gianni.zu07@gmail.com>
2024-09-20 16:14:28 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+// Imported Components
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+  components: {
+    AppHeader,
+    AppMain
+  }
+}
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
+
+<style lang="scss">
+@use "bootstrap/scss/bootstrap.scss" as *;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <h3>MAIN</h3>
+</template>
+
+<style scoped></style>
